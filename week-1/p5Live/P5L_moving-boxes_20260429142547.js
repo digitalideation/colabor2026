4eccbca2e432e5f929eb05c7e4a25f66b43cfe3a
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2026-04-29 17:55:16 +0200

    add material from day 2

---
+++ week-1/p5Live/P5L_moving-boxes_20260429142547.js
@@ -0,0 +1,44 @@
+// {"P5LIVE":{"name":"moving-boxes","mod":1777472747068}} 
+
+let posX = 0
+let posY = 0
+let boxDim = 100
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL)
+	
+}
+
+function draw() {
+	background(0)
+	// orbitControl()
+	fill(255)
+
+	// posX = posX + 1
+	// posX+=1
+	posX+=10
+	posY = posY - 10
+	if(posX > width/2 + boxDim){
+		// posX = 0
+		posX = -width/2
+	}
+	
+	if(posY < -(height / 2)){
+		posY = 0
+	}
+	
+	push()
+	translate(posX, posY, 0)
+	box(boxDim)
+	pop()
+	
+	strokeWeight(3)
+	push()
+	stroke(255, 0, 0)
+	line(0,0,0, width, 0, 0)
+	stroke(0, 255, 0)
+	line(0,0,0, 0, -height, 0)
+	stroke(0, 0, 255)
+	line(0,0,0, 0, 0, 1000)
+	pop()
+	
+}
